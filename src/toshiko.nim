d45f56c07b356337b33147d1698089e6803188b0
SakiKawasaki <social.ethosa@gmail.com>
2020-06-24 00:11:17 +0700

    add window settings in global settings.

--- src/toshiko.nim
+++ src/toshiko.nim
@@ -18,8 +18,28 @@ export
 
 # Read config
 if existsFile("global_settings.toshiko"):
-  var cfg = loadConfig("global_settings.toshiko")
+  var
+    cfg = loadConfig("global_settings.toshiko")
+    w: cint = 640
+    h: cint = 360
+
   setStandardFont(
     cfg.getSectionValue("GUI", "standard-font").cstring,
     parseInt(cfg.getSectionValue("GUI", "standard-font-size")).cint
   )
+
+  if cfg.getSectionValue("Window", "width") != "":
+    w = parseInt(cfg.getSectionValue("Window", "width")).cint
+  if cfg.getSectionValue("Window", "height") != "":
+    h = parseInt(cfg.getSectionValue("Window", "height")).cint
+
+  if w < 32:
+    w = 32
+  if h < 120:
+    h = 120
+
+  if cfg.getSectionValue("Window", "title") != "":
+    Window(cfg.getSectionValue("Window", "title").cstring, w, h)
+
+  if cfg.getSectionValue("Window", "icon") != "":
+    setIcon(cfg.getSectionValue("Window", "icon").cstring)
